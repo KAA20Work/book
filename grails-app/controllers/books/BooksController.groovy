4657f0b312c79b1cd4e67767960dd64513cdd818
KAA20Work <annet.f1limonova@yandex.com>
2022-11-10 15:21:48 +0500

    feat: made simple deleting - deleting first book

--- grails-app/controllers/books/BooksController.groovy
+++ grails-app/controllers/books/BooksController.groovy
@@ -4,14 +4,20 @@ import grails.converters.JSON
 
 
 class BooksController {
-    def index() {
+    def add() {
         def book = new Book(id: 1, title: "War and Piece", author: new Author(id: 1, name: "Lev Tolstoy"))
         book.save()
-
+        def book2 = new Book(id: 2, title: "Crime and Punishment", author: new Author(id: 2, name: "Dostoevsky"))
+        book2.save()
         redirect action: 'list'
     }
     def list() {
-        def books = Book.list()
+        def books = Book.list(sort:"title", order:"desc")
         render books as JSON
+    }
+    def delete() {
+        def book = Book.get(1)
+        book.delete(flush: true)
+        redirect action: 'list'
     }
 }
