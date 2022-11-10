cd752b0dba26c8ee503a1dcd2cee92858778911b
KAA20Work <annet.f1limonova@yandex.com>
2022-11-10 15:05:43 +0500

    feat: made simple query - prints list of books as JSON

--- grails-app/controllers/books/BooksController.groovy
+++ grails-app/controllers/books/BooksController.groovy
@@ -1,11 +1,17 @@
 package books
 
+import grails.converters.JSON
+
 
 class BooksController {
     def index() {
         def book = new Book(id: 1, title: "War and Piece", author: new Author(id: 1, name: "Lev Tolstoy"))
         book.save()
 
-        render "Saved successfully!"
+        redirect action: 'list'
+    }
+    def list() {
+        def books = Book.list()
+        render books as JSON
     }
 }
