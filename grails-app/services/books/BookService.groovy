48056955e1abbed03e1a67bac3269949d07ff084
KAA20Work <annet.f1limonova@yandex.com>
2022-11-21 19:58:58 +0500

    feat: made service scope as prototype (creating new instance in each injection)

---
+++ grails-app/services/books/BookService.groovy
@@ -0,0 +1,41 @@
+package books
+
+import grails.transaction.Transactional
+
+@Transactional
+class BookService {
+    static scope = "prototype"
+    @Transactional(readOnly = true)
+    def listBooks() {
+        Book.list()
+    }
+    @Transactional
+    def add() {
+        def book = new Book(id: 1, title: "War and Piece", author: new Author(id: 1, name: "Lev Tolstoy"))
+        book.save()
+        def book2 = new Book(id: 2, title: "Crime and Punishment", author: new Author(id: 2, name: "Dostoevsky"))
+        book2.save()
+    }
+    @Transactional
+    def delete() {
+        def book = Book.get(1)
+        book.delete(flush: true)
+    }
+    @Transactional
+    def update() {
+        def book = Book.get(1)
+        book.setTitle("War and War")
+        book.save(flush: true)
+    }
+    @Transactional
+    def find() {
+        def book = Book.findByTitle("Crime and Punishment")
+        return book
+    }
+    @Transactional
+    def findByTwoConditions() {
+        def query = Book.where{ title == "War and Piece" }
+        def query2 = query.where {id == 1}
+        return query2
+    }
+}
